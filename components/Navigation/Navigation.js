b83bb2f12433f9274744efd0eda4b8eb31bfe366
Adeel Butt <adeel.butt.hh@gmail.com>
2022-03-29 11:12:55 +0200

    added active Classlist to Nav

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,51 +1,54 @@
 import styled from "styled-components";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Navigation() {
+  const router = useRouter();
+
   return (
     <>
       <Nav>
-        <Link href="/">
-          <a>
+        <Link href="/" passHref>
+          <IconLink className={router.pathname == "/" ? "active" : ""}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              height="35px"
+              height="40px"
               viewBox="0 0 24 24"
-              width="35px"
+              width="40px"
               fill="#FFFFFF"
             >
               <path d="M0 0h24v24H0V0z" fill="none" />
               <path d="M10 19v-5h4v5c0 .55.45 1 1 1h3c.55 0 1-.45 1-1v-7h1.7c.46 0 .68-.57.33-.87L12.67 3.6c-.38-.34-.96-.34-1.34 0l-8.36 7.53c-.34.3-.13.87.33.87H5v7c0 .55.45 1 1 1h3c.55 0 1-.45 1-1z" />
             </svg>
-          </a>
+          </IconLink>
         </Link>
-        <Link href="/create">
-          <a>
+        <Link href="/create" passHref>
+          <IconLink className={router.pathname == "/create" ? "active" : ""}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              height="35px"
+              height="38px"
               viewBox="0 0 24 24"
-              width="35px"
+              width="38px"
               fill="#FFFFFF"
             >
               <path d="M0 0h24v24H0V0z" fill="none" />
               <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm4 11h-3v3c0 .55-.45 1-1 1s-1-.45-1-1v-3H8c-.55 0-1-.45-1-1s.45-1 1-1h3V8c0-.55.45-1 1-1s1 .45 1 1v3h3c.55 0 1 .45 1 1s-.45 1-1 1z" />
             </svg>
-          </a>
+          </IconLink>
         </Link>
-        <Link href="/profile">
-          <a>
+        <Link href="/profile" passHref>
+          <IconLink className={router.pathname == "/profile" ? "active" : ""}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              height="35px"
+              height="40px"
               viewBox="0 0 24 24"
-              width="35px"
+              width="40px"
               fill="#FFFFFF"
             >
               <path d="M0 0h24v24H0V0z" fill="none" />
               <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v1c0 .55.45 1 1 1h14c.55 0 1-.45 1-1v-1c0-2.66-5.33-4-8-4z" />
             </svg>
-          </a>
+          </IconLink>
         </Link>
       </Nav>
     </>
@@ -58,6 +61,13 @@ const Nav = styled.div`
   position: fixed;
   bottom: 0;
   width: 100%;
-  background-color: black;
-  padding: 10px;
+  background-color: darkblue;
+  padding: 15px;
+`;
+
+const IconLink = styled.a`
+  &.active {
+    border: solid white 2px;
+    border-radius: 5px;
+  }
 `;
