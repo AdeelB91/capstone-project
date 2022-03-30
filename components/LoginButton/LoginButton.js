6cfb42171719b5f72b287946400d0e868c97615f
Adeel Butt <adeel.butt.hh@gmail.com>
2022-03-30 13:30:18 +0200

    implemented and setup Login, created sign in button and styled the landingpage

--- components/LoginButton/LoginButton.js
+++ components/LoginButton/LoginButton.js
@@ -6,14 +6,12 @@ export default function LoginButton() {
   if (session) {
     return (
       <LoginContainer>
-        <span>Signed in</span>
         <button onClick={() => signOut()}>Sign out</button>
       </LoginContainer>
     );
   }
   return (
     <LoginContainer>
-      <span>Not signed in</span>
       <button onClick={() => signIn()}>Sign in</button>
     </LoginContainer>
   );
@@ -21,9 +19,33 @@ export default function LoginButton() {
 
 const LoginContainer = styled.div`
   display: flex;
-  flex-direction: column;
-  gap: 50px;
   justify-content: center;
+  margin-top: 50%;
+  > button {
+    width: fit-content;
+    box-shadow: 0px 1px 0px 0px #1c1b18;
+    background: linear-gradient(to bottom, #1a1404 5%, #1d508f 100%);
+    background-color: #1a1404;
+    border-radius: 18px;
+    border: 1px solid #141007;
+    display: inline-block;
+    cursor: pointer;
+    color: #ffffff;
+    font-family: Courier New;
+    font-size: 16px;
+    font-weight: normal;
+    padding: 6px 24px;
+    text-decoration: none;
+    text-shadow: 0px 1px 0px #000000;
+  }
+  .button:hover {
+    background: linear-gradient(to bottom, #1d508f 5%, #1a1404 100%);
+    background-color: #1d508f;
+  }
+  .button:active {
+    position: relative;
+    top: 1px;
+  }
 `;
 
 // {session.user.name}
