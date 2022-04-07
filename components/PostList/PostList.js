3d5e409363674f196c5c9f995b842ab2b0bb94f6
Adeel Butt <adeel.butt.hh@gmail.com>
2022-04-07 21:02:31 +0200

    styled categories

--- components/PostList/PostList.js
+++ components/PostList/PostList.js
@@ -11,17 +11,20 @@ export default function PostList({ type }) {
 
     const filterposts = posts.data.filter((post) => post.category === category);
     console.log(filterposts);
+
     // setFilterList(filterposts);
     // data.filter((data.category) => data.category === "Bücher" ).map()
   }
 
   return (
     <>
-      {categories.map((category) => (
-        <button onClick={() => handleFilter(category.name)} key={category.id}>
-          {category.name}
-        </button>
-      ))}
+      <CategoryBar>
+        {categories.map((category) => (
+          <button onClick={() => handleFilter(category.name)} key={category.id}>
+            {category.name}
+          </button>
+        ))}
+      </CategoryBar>
       {posts.data && Array.isArray(posts.data) ? (
         posts.data.length > 0 ? (
           <Ul>
@@ -50,5 +53,33 @@ const Ul = styled.ul`
   padding: 0;
   > li {
     flex: 1 0 30ch;
+  }
+`;
+
+const CategoryBar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 5vh;
+  margin: 2vh 0;
+  box-shadow: rgba(6, 24, 44, 0.4) 0px 0px 0px 2px,
+    rgba(6, 24, 44, 0.65) 0px 4px 6px -1px,
+    rgba(255, 255, 255, 0.08) 0px 1px 0px inset;
+  > button {
+    width: fit-content;
+    background-color: white;
+    border: none;
+    border-bottom: 1px solid;
+    padding: 1px;
+    cursor: pointer;
+
+    &:hover {
+      background-color: lightblue;
+    }
+    &:active {
+      background-color: lightblue;
+      border: 1px solid black;
+      padding: 2px;
+      font-size: medium;
+    }
   }
 `;
