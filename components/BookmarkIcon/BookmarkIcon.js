f243e3b81847a1f85ce18a0afbad38ce0f07aefb
Adeel Butt <adeel.butt.hh@gmail.com>
2022-04-21 14:54:31 +0200

    changed style in Bookmarkicon

--- components/BookmarkIcon/BookmarkIcon.js
+++ components/BookmarkIcon/BookmarkIcon.js
@@ -5,15 +5,22 @@ export default function BookmarkIcon({ active, handleClick }) {
   return (
     <>
       {active ? (
-        <BookmarkIcons onClick={() => handleClick()} />
+        <BookmarkDash onClick={() => handleClick()} />
       ) : (
-        <BookmarkIcons onClick={() => handleClick()} />
+        <BookmarkHeart onClick={() => handleClick()} />
       )}
     </>
   );
 }
 
-const BookmarkIcons = styled(BsBookmarkDash, BsBookmarkHeart)`
+const BookmarkDash = styled(BsBookmarkDash)`
+  cursor: pointer;
+  font-size: 25px;
+  @media screen and (min-width: 700px) {
+    font-size: 33px;
+  }
+`;
+const BookmarkHeart = styled(BsBookmarkHeart)`
   cursor: pointer;
   font-size: 25px;
   @media screen and (min-width: 700px) {
